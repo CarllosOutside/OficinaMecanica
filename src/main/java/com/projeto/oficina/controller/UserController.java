f3301dc405f4e98c5088c2bc98d9b890ec4b301d
CarllosMiranda <mirandac215@gmail.com>
2022-12-04 21:37:45 -0300

    postUser

--- src/main/java/com/projeto/oficina/controller/UserController.java
+++ src/main/java/com/projeto/oficina/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class UserController {
 	/*
 	 * GET USER
 	 * */
-	@GetMapping(path="/user/cred") //ENDEREÇO DE REQUISIÇÃO GET
+	@PostMapping(path="/user/cred") //ENDEREÇO DE REQUISIÇÃO GET
 	public ResponseEntity<UserModel> getUser(@RequestBody UserModel user) 
 	{
 		//PROCURA NO BANCO NA TABELA ESTADO
